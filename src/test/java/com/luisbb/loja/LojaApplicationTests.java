01094e68a81ab5666917344d3efa31f3ef787dd6
Luis Eduardo <luis10barbo2020@gmail.com>
2023-12-02 19:25:50 -0300

    Ola Mundo em Spring

---
+++ src/test/java/com/luisbb/loja/LojaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luisbb.loja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
