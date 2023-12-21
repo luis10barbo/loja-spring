78f91ea43fc8cbc7a4962a455e92728a302f0e06
Luis Eduardo <luis10barbo2020@gmail.com>
2023-12-21 18:15:47 -0300

    Adicionados previews de produto pagina inicial

--- client/src/app/routes/pagina-principal/pagina-principal.component.ts
+++ client/src/app/routes/pagina-principal/pagina-principal.component.ts
@@ -10,12 +10,24 @@ import { ServicoProduto } from '../../services/servico-produto.service';
 export class PaginaPrincipalComponent {
   title = 'loja generica';
   produtos: Produto[] = [];
+  preview1?: Produto;
+  preview2?: Produto;
 
   constructor(private servicoProduto: ServicoProduto) {}
   
   ngOnInit(): void {
     this.servicoProduto.adquirirTodos().subscribe(data => {
       this.produtos = data;
+      if (data[0]) {
+        this.preview1 = data[0];
+        
+        if (data[1]) {
+          this.preview2 = data[1];
+        }
+        else {
+          this.preview2 = data[0];
+        }
+      }
     })
   }
 }
